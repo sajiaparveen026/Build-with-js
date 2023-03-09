edb0f3ce77e02b8811886b82d3fd6cbf052ce728
sajiaparveen026 <100033460+sajiaparveen026@users.noreply.github.com>
2023-03-09 10:05:14 +0000

    committed

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const taskModal = document.querySelector(".task__modal_body");
 // console.log(taskModal);
 
 //****************Template for the card on */
+// elem indentifier key=${id} is been missing on line 15
 const htmlTaskContent = ({id,title, description , type, url})=>`
 <div class="col-md-6 col-lg-4  mt-3 " id=>${id}>
+
         <div class="card shadow-sm task__card">
+
         <div class="card-header d-flex justify-content-end task__card__header">
            <button type="button" class='btn btn-outline-info' name=${id}>
            <i class='fas fa-pencil-alt mr-1.5' name=${id}></i>
@@ -43,4 +46,30 @@ const htmlTaskContent = ({id,title, description , type, url})=>`
 `;
 
 //Modal body on >> clk on open task
-const htmlTaskContent = ({id,title, description , url})=>
+const htmlModalContent = ({id, title , description , url })=>{
+const date = new Date(parseInt(id));
+ return `
+   <div id=${id}>
+   ${
+      url &&
+      ` <img  alt='Card image' width='100%' src=${url} class='img-fluid place__holder__image mb-3' />`
+     }
+     <strong class='text-muted text-sm'>Created on: ${date.toDateString()}</strong>
+     <h2 clas='my-3'>${title}</h2>
+      <p class='text-muted'>${description}</p> 
+   </div>
+ `
+};
+
+const updateLocalStorage = () =>
+{
+   localStorage.setItem(
+      "taky",
+      JSON.stringify({
+         tasks: state.taskList,
+      })
+   );
+};
+
+//load initial data
+//
