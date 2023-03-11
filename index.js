41da0e7ab9d0b32605ff25a3970037cdef05c333
sajiaparveen026 <100033460+sajiaparveen026@users.noreply.github.com>
2023-03-11 09:07:29 +0000

    created open task

--- index.js
+++ index.js
@@ -1,101 +1,132 @@
+
+// backup storage
 const state = {
-    taskList: [],
-};
-
-//DOM operations here
-const taskContents = document.querySelector(".task__contents");
-const taskModal = document.querySelector(".task__modal_body");
-
-// console.log(taskContents);
-// console.log(taskModal);
-
-//****************Template for the card on */
-// elem indentifier key=${id} is been missing on line 15
-const htmlTaskContent = ({id,title, description , type, url})=>`
-<div class="col-md-6 col-lg-4  mt-3 " id=>${id}>
-
-        <div class="card shadow-sm task__card">
-
-        <div class="card-header d-flex justify-content-end task__card__header">
-           <button type="button" class='btn btn-outline-info' name=${id}>
-           <i class='fas fa-pencil-alt mr-1.5' name=${id}></i>
+   taskList: [],
+ };
+ 
+ // DOM Operations
+ const taskModal = document.querySelector(".task__modal__body");
+ const taskContents = document.querySelector(".task__contents");
+ 
+ // console.log(taskContents);
+ // console.log(taskModal);
+ 
+ // Template for the card on screen
+ // elem identifier key=${id} is been misssing on line 50th
+ const htmlTaskContent = ({ id, title, description, type, url }) => `
+   <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
+     <div class='card shadow-sm task__card'>
+     
+       <div class='card-header d-flex justify-content-end task__card__header'>
+           <button type='button' class='btn btn-outline-primary mr-1.5' name=${id}>
+               <i class='fas fa-pencil-alt name=${id}'></i>
            </button>
-           <button type="button" class='btn btn-outline-danger' name=${id}>
-           <i class='fas fa-trash-alt mr-1.5' name=${id}></i>
+            <button type='button' class='btn btn-outline-danger mr-1.5' name=${id} onclick='deleteTask.apply(this , arguments)'>
+               <i class='fas fa-trash-alt name=${id}' onclick='deleteTask.apply(this , arguments)'></i>
            </button>
-        </div>
-
-        <div class='card-body'>
+       </div>
+       <div class='card-body'>
            ${
-            url &&
-            ` <img  alt='Card image' width='100%' src=${url} class='card-img-top md-3 rounded-lg' />`
+            //  url &&
+            //  `<img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg' />`
+            url ? 
+            `<img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg' />`
+            :`<img width='100%' src="https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png" alt='Card Image' class='card-img-top md-3 rounded-lg' />`
            }
-           <h4 class='card-title task__card_title'>${title}</h4>
+           <h4 class='card-title task__card__title'>${title}</h4>
            <p class='description trim-3-lines text-muted'>${description}</p>
-           <div class='tags'>
-           <span class='badge bg-primary m-1'>${type}</span>
+           <div class='tags text-white d-flex flex-wrap'>
+             <span> class='badge bg-primary m-1'${type}</span>
            </div>
-        </div>
-
-        <div class='card-footer'>
-        <button type='button' class='btn btn-outline-primary float-right'
-        data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
-        </div>
-        </div>
-      </div>
-`;
-
-//Modal body on >> clk on open task
-const htmlModalContent = ({id, title , description , url })=>{
-const date = new Date(parseInt(id));
- return `
-   <div id=${id}>
-   ${
-      url &&
-      ` <img  alt='Card image' width='100%' src=${url} class='img-fluid place__holder__image mb-3' />`
-     }
-     <strong class='text-muted text-sm'>Created on: ${date.toDateString()}</strong>
-     <h2 clas='my-3'>${title}</h2>
-      <p class='text-muted'>${description}</p> 
+       </div>
+       <div class='card-footer'>
+           <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask" onclick='openTask()' id=${id}>Open Task</button>
+       </div>
+     </div>
    </div>
- `
-};
-
-// here we convert Json to string(for local storage)
-const updateLocalStorage = () =>
-{
+ `;
+ 
+ // Modal Body on >> Clk of Open Task
+ const htmlModalContent = ({ id, title, description, url }) => {
+   const date = new Date(parseInt(id));
+   return `
+   <div id=${id}>
+      ${
+         url ? 
+         `<img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg' />`
+         :`<img width='100%' src="https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png" alt='Card Image' class='card-img-top md-3 rounded-lg' />`
+        }
+      <strong class='text-muted text-sm'>Created on: ${date.toDateString()}</strong>
+      <h2 class='my-3'>${title}</h2>
+      <p class='text-muted'>${description}</p>
+   </div>
+   `;
+ };
+ 
+ // where we convert json > str (i.e., for local storage)
+ const updateLocalStorage = () => {
    localStorage.setItem(
-      "task",
-      JSON.stringify({
-         tasks: state.taskList,
-      })
+     "task",
+     JSON.stringify({
+       tasks: state.taskList,
+     })
    );
-};
-
-//load initial data
-//here we convert back from string to Json(for rendering the cards on the screen)
-const loadInitialData = () =>{
-   const localStorageCopy = JSON.parse(localStorage.tasks);
-
-   if(localStorageCopy)
-   {
-      state.taskList = localStorageCopy.tasks;
+ };
+ 
+ 
+ const loadInitialData = () => {
+   const localStorageCopy = JSON.parse(localStorage.task);
+ 
+   if (localStorageCopy) state.taskList = localStorageCopy.tasks;
+ 
+   state.taskList.map((cardDate) => {
+     taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+   });
+ };
+ 
+ 
+ 
+ // when we update or when we edit ..we need to save
+ const handleSubmit = (event) => {
+   // console.log("event triggerd");
+   const id = `${Date.now()}`;
+   const input = {
+     url: document.getElementById("imageUrl").value,
+     title: document.getElementById("taskTitle").value,
+     type: document.getElementById("tags").value,
+     description: document.getElementById("taskDescription").value,
+   };
+   if (input.title === "" || input.type === "" || input.description === "") {
+     return alert("Please fill all the necessary fiels :-)");
    }
-  
-   state.taskList.map((cardDate)=>{
-      taskContents.innerAdjacentHTML("beforeend",htmlTaskContent(cardDate));
-   })
-};
+ 
+   // taskContents.innerAdjacentHTML(
+   taskContents.insertAdjacentHTML(
+     "beforeend",
+     htmlTaskContent({ ...input, id })
+   );
+   state.taskList.push({ ...input, id });
+ 
+   updateLocalStorage();
+ };
+ 
+ //open task
+const openTask = (e) =>{
+   if(!e) e=window.event;
 
-//when we update/edit... we need to save
-const handleSubmit = (event) =>{
-const id = `${Date.now()}`;
-const input = {
-   url : document.getElementById("imageUrl").value,
-  title: document.getElementById("taskTitle").value,
-  tags: document.getElementById("tags").value,
-  taskDescription: document.getElementById("taskDesc").value,
-};
-taskContents.innerAdjacentHTML("beforeend",htmlTaskContent({...input,id}));
+   const getTask = state.taskList.find(({id}) => id ===e.target.id);
+   taskModal.innerHTML = htmlModalContent(getTask);
+}
+  //delete task
+  const deleteTask = (e) =>{
+   if(!e) e=window.event;
 
-};
+   const targetId = e.target.getAttribute("name");
+  // console.log(targetId);
+
+
+}
+
+ // edit task
+ // save edit
+ // search
